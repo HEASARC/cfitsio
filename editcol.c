d32c82f49ef2e8c8e464b62ffec2def07ff8e7de
gordon <>
2015-10-30 20:06:05 +0000

    Added one more check to the previous check-in: make sure the HDUpositions of the infptr and outfptr match.  Don't want to increment 'incol' if the insertion comes from a different HDU within the same file.

--- editcol.c
+++ editcol.c
@@ -1591,7 +1591,9 @@ int ffcpcl(fitsfile *infptr,    /* I - FITS file pointer to input file  */
            return(*status);
         }
 
-        if (((infptr->Fptr) == (outfptr->Fptr)) && (colnum <= incol)) 
+        if (((infptr->Fptr) == (outfptr->Fptr)) 
+           && (infptr->HDUposition == outfptr->HDUposition)
+           && (colnum <= incol)) 
 	    incol++;  /* the input column has been shifted over */
 
         /* copy the comment strings from the input file for TTYPE and TFORM */
