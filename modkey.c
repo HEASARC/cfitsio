587e156ac2639fe5a6657e082ec496c50fbb9035
Craig Gordon <craig.a.gordon@nasa.gov>
2018-03-26 14:20:53 -0400

    Fix to ffikey.  It had been improperly capitalizing letters before an '=' sign on a CONTINUE line.  I used the same fix that already existed in the ffprec function.

--- modkey.c
+++ modkey.c
@@ -1531,6 +1531,11 @@ int ffikey(fitsfile *fptr,    /* I - FITS file pointer  */
     keylength = strcspn(buff2, "=");
     if (keylength == 80) keylength = 8;
     
+    /* test for the common commentary keywords which by definition have 8-char names */
+    if ( !fits_strncasecmp( "COMMENT ", buff2, 8) || !fits_strncasecmp( "HISTORY ", buff2, 8) ||
+         !fits_strncasecmp( "        ", buff2, 8) || !fits_strncasecmp( "CONTINUE", buff2, 8) )
+	 keylength = 8;
+
     for (ii=0; ii < keylength; ii++)       /* make sure keyword name is uppercase */
         buff2[ii] = toupper(buff2[ii]);
 
