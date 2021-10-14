c3a4f2762ad4281101c1de5768c7e40cc8b1ba0e
Bryan Irby <bryan.k.irby@nasa.gov>
2021-10-14 16:25:54 -0400

    Various cmake updates.
    Expanded conditional MSVC curl check to all of WIN32 to prevent MinGW conflicts with TBYTE; set PKG_CONFIG_LIBS as needed to insert dependencies (e.g. curl, zlib) into the pkg-config file; aligned some build options with the documentation; aligned version strings with the main configure script for clarity.

--- cfitsio.pc.cmake
+++ cfitsio.pc.cmake
@@ -7,6 +7,6 @@ Name: cfitsio
 Description: FITS File Subroutine Library
 URL: https://heasarc.gsfc.nasa.gov/fitsio/
 Version: @CFITSIO_MAJOR@.@CFITSIO_MINOR@.@CFITSIO_MICRO@
-Libs: -L${libdir} @PKG_CONFIG_LIBS@
+Libs: -L${libdir} -lcfitsio @PKG_CONFIG_LIBS@
 Libs.private: -lm
 Cflags: -I${includedir}
