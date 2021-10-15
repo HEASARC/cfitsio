16a208404c27f4e484d2afe2311de9a1c0c91bd5
Craig Gordon <craig.a.gordon@nasa.gov>
2021-10-15 13:08:22 -0400

    No interface change: Comments change only
    Added warning to CFITSIO_MICRO and CFITSIO_MINOR defs that they
    must not exceed 99.  This limitation is imposed by ffvers() function.

--- fitsio.h
+++ fitsio.h
@@ -35,6 +35,8 @@ SERVICES PROVIDED HEREUNDER."
 #define _FITSIO_H
 
 #define CFITSIO_VERSION 4.0.0
+/* Minor and micro numbers must not exceed 99 under current method
+   of version representataion in ffvers(). */
 #define CFITSIO_MICRO 0
 #define CFITSIO_MINOR 0
 #define CFITSIO_MAJOR 4
