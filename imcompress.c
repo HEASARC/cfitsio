87608f0059bf3e347fb43e2bb54d67c3af1f2a3f
Craig Gordon <craig.a.gordon@nasa.gov>
2023-12-04 13:21:29 -0500

    Typo fix to error message in fits_compress_table.  Originally submitted by aurel32 through github.

--- imcompress.c
+++ imcompress.c
@@ -8630,7 +8630,7 @@ int fits_compress_table(fitsfile *infptr, fitsfile *outfptr, int *status)
   	            dlen = fits_rcomp_byte ((signed char *)(cm_buffer + cm_colstart[ii]), datasize, (unsigned char *) cvlamem,
                        datasize * 2, 32);
 	        } else {  /* this should not happen */
-                    ffpmsg(" Error: cannot compress this column type with the RICE algorthm");
+                    ffpmsg(" Error: cannot compress this column type with the RICE algorithm");
 		    free(cvlamem);  free(cm_buffer);
 	            *status = DATA_COMPRESSION_ERR;
 	            return(*status);
